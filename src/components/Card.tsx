a3d9520d9a1f5e220e8b3558ab0b4dc22aff5937
Joe Knox <joseph.g.knox@gmail.com>
2024-02-23 12:20:22 -0800

    cleanup

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,7 +14,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, description }) => {
   return (
-    <StyledView className="bg-gray-600 w-64 mr-4 p-4 rounded-lg">
+    <StyledView className="bg-gray-200 w-64 mr-5 p-4 rounded-md">
       <StyledText className="text-black text-lg font-bold mb-2">{title}</StyledText>
       <StyledText className="text-black">{description}</StyledText>
     </StyledView>
